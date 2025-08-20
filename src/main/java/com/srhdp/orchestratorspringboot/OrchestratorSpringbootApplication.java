7ee1f90a794f07ee1d1b81222faf2d864ac6d77a
srhdp <srhdp@BRN-MacBook-Pro.local>
2025-08-20 09:37:01 +0700

    init project

---
+++ src/main/java/com/srhdp/orchestratorspringboot/OrchestratorSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.srhdp.orchestratorspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestratorSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrchestratorSpringbootApplication.class, args);
+    }
+
+}
