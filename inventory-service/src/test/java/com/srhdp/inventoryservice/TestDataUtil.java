488a0a9acbe8fc5f3717ae800d48783d455e1a1c
srhdp <srhdp@BRN-MacBook-Pro.local>
2025-08-21 10:01:25 +0700

    inventory integrate test

---
+++ inventory-service/src/test/java/com/srhdp/inventoryservice/TestDataUtil.java
@@ -0,0 +1,23 @@
+package com.srhdp.inventoryservice;
+
+import com.srhdp.orchestratorcommon.messages.inventory.InventoryRequest;
+
+import java.util.UUID;
+
+public class TestDataUtil {
+
+    public static InventoryRequest createDeductRequest(UUID orderId, int productId, int quantity) {
+        return InventoryRequest.Deduct.builder()
+                .orderId(orderId)
+                .productId(productId)
+                .quantity(quantity)
+                .build();
+    }
+
+    public static InventoryRequest createRestoreRequest(UUID orderId) {
+        return InventoryRequest.Restore.builder()
+                .orderId(orderId)
+                .build();
+    }
+
+}
